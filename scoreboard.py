b0fb4fbe8e2c2dadce77b868d224ffa72073924d
nthumu <44855085+nthumu@users.noreply.github.com>
2022-02-11 16:58:55 +0530

    Changes made to open while and read high score from it and write the highest score.

--- scoreboard.py
+++ scoreboard.py
@@ -9,7 +9,8 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt", "r") as file:
+            self.high_score = int(file.read())
         self.color("white")
         self.hideturtle()
         self.penup()
@@ -23,6 +24,8 @@ class ScoreBoard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", "w") as high_file:
+                high_file.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
 
